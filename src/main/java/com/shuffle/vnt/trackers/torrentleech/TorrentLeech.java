181779a380b1f1ccae0c4e9934769f04ba4ff293
Cristian Moro <cristianmoro@gmail.com>
2016-01-07 00:00:59 -0200

    add support tl

---
+++ src/main/java/com/shuffle/vnt/trackers/torrentleech/TorrentLeech.java
@@ -0,0 +1,160 @@
+package com.shuffle.vnt.trackers.torrentleech;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.jsoup.Jsoup;
+
+import com.shuffle.vnt.core.parser.TorrentDetailedParser;
+import com.shuffle.vnt.core.parser.TorrentParser;
+import com.shuffle.vnt.core.parser.TrackerConfig;
+import com.shuffle.vnt.core.parser.bean.Body;
+import com.shuffle.vnt.core.parser.bean.TrackerCategory;
+
+public class TorrentLeech implements TrackerConfig {
+
+	private final String name = "TorrentLeech";
+
+	private final String url = "https://www.torrentleech.org/torrents/browse/index/";
+
+	private final String authenticationUrl = "https://www.torrentleech.org/user/account/login/";
+	
+	private final ParameterType parameterType = ParameterType.PATH;
+
+	private final String usernameField = "username";
+
+	private final String passwordField = "password";
+
+	private final String authenticationMethod = "POST";
+
+	private final String pageField = "page";
+
+	private final String searchField = "query";
+
+	private final String categoryField = "categories";
+	
+	private final List<TrackerCategory> categories = new ArrayList<>();
+	
+	{
+	    categories.add(new TrackerCategory("Movies: Cam", "", "8"));
+	    categories.add(new TrackerCategory("Movies: TS/TC", "", "9"));
+	    categories.add(new TrackerCategory("Movies: R5/Screeners", "", "10"));
+	    categories.add(new TrackerCategory("Movies: DVDRip/DVDScreener", "", "11"));
+	    categories.add(new TrackerCategory("Movies: DVD-R", "", "12"));
+	    categories.add(new TrackerCategory("Movies: HD", "", "13"));
+	    categories.add(new TrackerCategory("Movies: BDRip", "", "14"));
+	    categories.add(new TrackerCategory("Movies: Boxsets", "", "15"));
+	    categories.add(new TrackerCategory("Movies: Documentaries", "", "29"));
+	    
+	    categories.add(new TrackerCategory("TV: Episodes", "", "26"));
+	    categories.add(new TrackerCategory("TV: BoxSets", "", "27"));
+	    categories.add(new TrackerCategory("TV: Episodes HD", "", "32"));
+	    
+	    categories.add(new TrackerCategory("Games: PC", "", "17"));
+	    categories.add(new TrackerCategory("Games: XBOX", "", "18"));
+	    categories.add(new TrackerCategory("Games: XBOX360", "", "19"));
+	    categories.add(new TrackerCategory("Games: PS2", "", "20"));
+	    categories.add(new TrackerCategory("Games: PS3", "", "21"));
+	    categories.add(new TrackerCategory("Games: PSP", "", "22"));
+	    categories.add(new TrackerCategory("Games: Wii", "", "28"));
+	    categories.add(new TrackerCategory("Games: Nintendo DS", "", "30"));
+	    
+	    categories.add(new TrackerCategory("Music: Music Videos", "", "16"));
+	    categories.add(new TrackerCategory("Music: Audio", "", "31"));
+	    
+	    categories.add(new TrackerCategory("Animation: Anime", "", "34"));
+	    categories.add(new TrackerCategory("Animation: Cartoons", "", "35"));
+	    
+	    categories.add(new TrackerCategory("Books", "", "5"));
+	    
+	    categories.add(new TrackerCategory("Applications: PC-ISO", "", "23"));
+	    categories.add(new TrackerCategory("Applications: Mac", "", "24"));
+	    categories.add(new TrackerCategory("Applications: PDA", "", "25"));
+	    categories.add(new TrackerCategory("Applications: 0-day", "", "33"));
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String getUrl() {
+		return url;
+	}
+
+	@Override
+	public String getAuthenticationUrl() {
+		return authenticationUrl;
+	}
+	
+	@Override
+	public ParameterType getParameterType() {
+	    return parameterType;
+	}
+
+	@Override
+	public boolean isAuthenticated(Body body) {
+		return Jsoup.parse(body.getContent()).select("#loginform").size() <= 0;
+	}
+
+	@Override
+	public String getUsernameField() {
+		return usernameField;
+	}
+
+	@Override
+	public String getPasswordField() {
+		return passwordField;
+	}
+
+	@Override
+	public String getAuthenticationMethod() {
+		return authenticationMethod;
+	}
+
+	@Override
+	public Map<String, String> getAuthenticationAdditionalParameters() {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("login", "submit");
+		return map;
+	}
+
+	@Override
+	public String getPageField() {
+		return pageField;
+	}
+
+	@Override
+	public String getSearchField() {
+		return searchField;
+	}
+
+	@Override
+	public String getCategoryField() {
+		return categoryField;
+	}
+
+	@Override
+	public List<TrackerCategory> getCategories() {
+	    return categories;
+	}
+
+	@Override
+	public String getPageValue(long page) {
+		return String.valueOf(page + 1);
+	}
+
+	@Override
+	public TorrentParser getTorrentParser() {
+		return new TorrentLeechTorrent();
+	}
+
+	@Override
+	public TorrentDetailedParser getTorrentDetailedParser() {
+		return new TorrentLeechTorrentDetail();
+	}
+
+}
---
+++ src/main/java/com/shuffle/vnt/trackers/torrentleech/TorrentLeechTorrent.java
@@ -0,0 +1,87 @@
+package com.shuffle.vnt.trackers.torrentleech;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+
+import com.shuffle.vnt.core.parser.TorrentParser;
+import com.shuffle.vnt.core.parser.bean.Body;
+import com.shuffle.vnt.core.parser.bean.Row;
+import com.shuffle.vnt.util.VntUtil;
+
+public class TorrentLeechTorrent implements TorrentParser {
+    
+    private final String baseUrl = "https://www.torrentleech.org/";
+
+    @Override
+    public List<Row> getRows(Body body) {
+	List<Row> rows = new ArrayList<Row>();
+	for (Element element : Jsoup.parse(body.getContent()).select("#torrenttable > tbody > tr[id]")) {
+	    Row row = new Row();
+	    row.setContent(element.outerHtml());
+	    rows.add(row);
+	}
+	return rows;
+    }
+
+    @Override
+    public long getId(Row row) {
+	return Long.valueOf(Jsoup.parse(getFullContentToParse(row)).select("tr").attr("id"));
+    }
+
+    @Override
+    public String getNome(Row row) {
+	return Jsoup.parse(getFullContentToParse(row)).select("tr > td.name > span.title > a").first().text();
+    }
+
+    @Override
+    public String getLink(Row row) {
+	return baseUrl + Jsoup.parse(getFullContentToParse(row)).select("tr > td.name > span.title > a").first().attr("href");
+    }
+
+    @Override
+    public String getDownlodLink(Row row) {
+	return baseUrl + Jsoup.parse(getFullContentToParse(row)).select("tr > td.quickdownload > a").first().attr("href");
+    }
+
+    private String getFullContentToParse(Row row) {
+	StringBuilder rowContent = new StringBuilder();
+	rowContent.append("<table>");
+	rowContent.append(row.getContent());
+	rowContent.append("</table>");
+	return rowContent.toString();
+    }
+
+    @Override
+    public double getSize(Row row) {
+	return VntUtil.parseSize(Jsoup.parse(getFullContentToParse(row)).select("tr > td:nth-child(5)").first().text().replaceAll(",", ""));
+    }
+
+    @Override
+    public Date getAdded(Row row) {
+	DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	String textFull = Jsoup.parse(getFullContentToParse(row)).select("tr > td.name").first().html();
+	String afterBr = textFull.split("<br>")[1];
+	String dateString = afterBr.split("on ")[afterBr.split("on ").length - 1];
+	Date date = null;
+	try {
+	    date = dateFormat.parse(dateString);
+	}
+	catch (ParseException e) {
+	    e.printStackTrace();
+	}
+	return date;
+    }
+
+    @Override
+    public String getCategory(Row row) {
+	return Jsoup.parse(getFullContentToParse(row)).select("tr > td.name > b").first().text();
+    }
+
+}
